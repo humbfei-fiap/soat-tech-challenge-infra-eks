bbed92afba12ef057a1663dc9c3892036abed579
humberto moura <humbertomoura@gmail.com>
2025-08-30 15:44:20 -0300

    variavel eks

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,7 +34,8 @@ module "eks" {
   cluster_version = "1.34"
 
   vpc_id     = module.vpc.vpc_id
-
+  subnet_ids = module.vpc.private_subnets
+  
   eks_managed_node_groups = {
     one = {
       name           = "${var.cluster_name}-node-group"
