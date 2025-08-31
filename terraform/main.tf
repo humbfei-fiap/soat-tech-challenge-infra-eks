50bd412eae54d6f98631c58658a627e59387a493
humberto moura <humbertomoura@gmail.com>
2025-08-31 13:13:18 -0300

    Corrigindo arquivos terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,7 +34,7 @@ module "eks" {
   cluster_version = "1.30"
   vpc_id          = module.vpc.vpc_id
   subnet_ids      = module.vpc.private_subnets
-  
+
   eks_managed_node_groups = {
     # Define um node group. A chave "default" identifica este grupo no mapa.
     default = {
@@ -49,5 +49,4 @@ module "eks" {
       }
     }
   }
-
 }
