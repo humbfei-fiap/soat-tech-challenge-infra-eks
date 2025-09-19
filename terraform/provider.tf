fee9b8e2f72c28852a3ad0e8791da336eeebd2d5
humberto moura <humbertomoura@gmail.com>
2025-09-19 20:23:24 -0300

    fix terraform

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_version = ">= 1.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "meu-eks-terraform-state--use1-az4--x-s3" # SUBSTITUA
+    key            = "global/eks/terraform.tfstate"
+    region         = "us-east-1" # SUBSTITUA
+    dynamodb_table = "meu-eks-terraform-lock" # SUBSTITUA
+    encrypt        = true
+  }
+}
+
 provider "aws" {
-  region = var.region
+  region = var.aws_region
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,53 +1,17 @@
-variable "region" {
-  description = "AWS region"
+variable "aws_region" {
+  description = "Região da AWS para criar os recursos"
   type        = string
   default     = "us-east-1"
 }
 
 variable "cluster_name" {
-  description = "Name of the EKS cluster"
+  description = "Nome do cluster EKS"
   type        = string
-  default     = "soat-tech-challenge-eks-cluster"
+  default     = "eks-techchallenger"
 }
 
-variable "vpc_cidr_block" {
-  description = "CIDR block for the VPC"
+variable "vpc_cidr" {
+  description = "Bloco CIDR para a VPC"
   type        = string
   default     = "10.0.0.0/16"
-}
-
-variable "public_subnet_cidrs" {
-  description = "List of CIDR blocks for public subnets"
-  type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
-}
-
-variable "private_subnet_cidrs" {
-  description = "List of CIDR blocks for private subnets"
-  type        = list(string)
-  default     = ["10.0.10.0/24", "10.0.11.0/24"]
-}
-
-variable "instance_type" {
-  description = "EC2 instance type for worker nodes"
-  type        = string
-  default     = "t2.medium"
-}
-
-variable "desired_size" {
-  description = "Desired number of worker nodes"
-  type        = number
-  default     = 1
-}
-
-variable "max_size" {
-  description = "Maximum number of worker nodes"
-  type        = number
-  default     = 2
-}
-
-variable "min_size" {
-  description = "Minimum number of worker nodes"
-  type        = number
-  default     = 1
 }
