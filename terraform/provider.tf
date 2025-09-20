22b12d59e33dd7aa57e39c00cefc4b2f5d63ba41
humberto moura <humbertomoura@gmail.com>
2025-09-19 22:01:25 -0300

    fix output

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -9,10 +9,10 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "meu-eks-terraform-state" # SUBSTITUA
+    bucket         = "meu-eks-terraform-state" # 
     key            = "global/eks/terraform.tfstate"
     region         = "us-east-1" # SUBSTITUA
-    dynamodb_table = "meu-eks-terraform-lock" # SUBSTITUA
+    dynamodb_table = "meu-eks-terraform-lock-001" # 
     encrypt        = true
   }
 }
