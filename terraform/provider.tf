52ca29768002ad5504e45662508410db330e33cb
humberto moura <humbertomoura@gmail.com>
2025-09-28 19:04:09 -0300

    fix corigindo eks

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -37,7 +37,7 @@ provider "kubernetes" {
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
+    args        = ["eks", "get-token", "--cluster-name", local.cluster_name]
   }
 }
 
@@ -50,7 +50,7 @@ provider "helm" {
     exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
+      args        = ["eks", "get-token", "--cluster-name", local.cluster_name]
     }
   }
 }
